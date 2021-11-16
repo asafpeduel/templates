bf0685d5170eb4ef94da614fc7ad39f3f6606d1d
asafpeduel <87314359+asafpeduel@users.noreply.github.com>
2021-11-16 22:10:58 +0200

    Update some.tf

--- some.tf
+++ some.tf
@@ -4,3 +4,10 @@ variable "availability_zone_names" {
   description = "variable description"
   sensitive = true
 }
+
+variable "availability_zone_names" {
+  default= "44"
+  type = string
+  description = "variable description"
+  sensitive = true
+}
